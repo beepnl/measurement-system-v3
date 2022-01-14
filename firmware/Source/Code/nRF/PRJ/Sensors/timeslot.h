5007611058ad03299d825e00482c150a091965ef
Pim van Gennip <pim@iconize.nl>
2022-01-14 13:27:57 +0100

    Move firmware to own repo

--- firmware/Source/Code/nRF/PRJ/Sensors/timeslot.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMESLOT_H
-#define	TIMESLOT_H
-	#include <stdint.h>
-	#include <stdbool.h>
-
-
-#endif	/* TIMESLOT_H */
-
--- firmware/Source/Code/nRF/Util/erase.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-ECHO Erase 
-start /B /wait nrfjprog --recover
-start /B /wait nrfjprog --eraseall
-start /B /wait nrfjprog --reset
-ECHO Erasing Done
-
-pause
